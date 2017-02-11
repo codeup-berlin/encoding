b9a604a92de962366beb04a7956a88407e8fdde6
Schwalbe Sophie <sophie.schwalbe@digitalperformance.de>
2017-02-11 13:23:40 +0100

    phpdoc corrected

--- src/Decoder.php
+++ src/Decoder.php
@@ -6,6 +6,7 @@ interface Decoder
     /**
      * @param string $data
      * @return string
+     * @throws \InvalidArgumentException if the passed data can not be decoded
      */
     public function decode(string $data): string;
 }
--- src/Encoder.php
+++ src/Encoder.php
@@ -6,7 +6,6 @@ interface Encoder
     /**
      * @param string $data
      * @return string
-     * @throws \InvalidArgumentException if the passed data can not be decoded
      */
     public function encode(string $data): string;
 }
