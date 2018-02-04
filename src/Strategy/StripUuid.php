06910153499425e5179ead293f1be5a9b50967c0
Schwalbe Sophie <sophie@codeup.berlin>
2018-02-04 16:23:23 +0100

    strip uuid strategy added

---
+++ src/Strategy/StripUuid.php
@@ -0,0 +1,36 @@
+<?php
+namespace Codeup\Encoding\Strategy;
+
+class StripUuid implements \Codeup\Encoding\Strategy
+{
+    /**
+     * @param string $data
+     * @return string
+     */
+    public function encode(string $data): string
+    {
+        if (!preg_match('/^[a-f0-9]{8}\-([a-f0-9]{4}\-){3}[a-f0-9]{12}$/', $data)) {
+            throw new \InvalidArgumentException('Not a valid uuid: ' . $data);
+        }
+        return str_replace('-', '', $data);
+    }
+
+    /**
+     * @param string $data
+     * @return string
+     * @throws \InvalidArgumentException if the passed data can not be decoded
+     */
+    public function decode(string $data): string
+    {
+        if (!preg_match('/^[a-f0-9]{32}$/', $data)) {
+            throw new \InvalidArgumentException('Not a stripped uuid: ' . $data);
+        }
+        return implode('-', [
+            substr($data, 0, 8),
+            substr($data, 8, 4),
+            substr($data, 12, 4),
+            substr($data, 16, 4),
+            substr($data, 20),
+        ]);
+    }
+}
---
+++ tests/Strategy/StripUuidTest.php
@@ -0,0 +1,76 @@
+<?php
+
+namespace Codeup\Encoding\Strategy;
+
+class StripUuidTest extends \PHPUnit\Framework\TestCase
+{
+    /**
+     * @test
+     */
+    public function encode_uuid()
+    {
+        $classUnderTest = new StripUuid();
+        $result = $classUnderTest->encode('364f1e0d-a2ca-4e83-8139-26b058df27fe');
+        $this->assertSame('364f1e0da2ca4e83813926b058df27fe', $result);
+    }
+
+    /**
+     * @return array
+     */
+    public function provideNonUuidValues()
+    {
+        return [
+            'missing char' => ['364f1e0d-a2ca-4e83-8139-26b058df27f'],
+            'stripped uuid' => ['364f1e0da2ca4e83813926b058df27fe'],
+            'invalid char' => ['364f1e0d-a2ca-4e83-8139-26b058df27g'],
+            'number' => [234],
+            'random' => [uniqid()],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider provideNonUuidValues
+     * @expectedException \InvalidArgumentException
+     */
+    public function encode_nonUuid($value)
+    {
+        $classUnderTest = new StripUuid();
+        $classUnderTest->encode($value);
+    }
+
+    /**
+     * @test
+     */
+    public function decode_strippedUuid()
+    {
+        $classUnderTest = new StripUuid();
+        $result = $classUnderTest->decode('364f1e0da2ca4e83813926b058df27fe');
+        $this->assertSame('364f1e0d-a2ca-4e83-8139-26b058df27fe', $result);
+    }
+
+    /**
+     * @return array
+     */
+    public function provideNonStrippedUuidValues()
+    {
+        return [
+            'missing char' => ['364f1e0d-a2ca-4e83-8139-26b058df27f'],
+            'unstripped uuid' => ['364f1e0d-a2ca-4e83-8139-26b058df27fe'],
+            'invalid char' => ['364f1e0da2ca4e83813926b058df27g'],
+            'number' => [234],
+            'random' => [uniqid()],
+        ];
+    }
+
+    /**
+     * @test
+     * @dataProvider provideNonStrippedUuidValues
+     * @expectedException \InvalidArgumentException
+     */
+    public function decode_nonUuid($value)
+    {
+        $classUnderTest = new StripUuid();
+        $classUnderTest->decode($value);
+    }
+}
